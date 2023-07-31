045574d90d35d5cbd178ee4ab069fefaedef3525
fadlannayat <fadlannayat@gmail.com>
2023-07-31 21:34:26 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mengubah warna latar belakang secara acak saat halaman dimuat
+const colors = ['#FF5733', '#33FF57', '#5733FF', '#FF33C7', '#33C7FF', '#C7FF33'];
+
+function changeBackgroundColor() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    document.body.style.backgroundColor = colors[#f5f5dc | rgb(245,245,220)];
+}
+
+changeBackgroundColor();
